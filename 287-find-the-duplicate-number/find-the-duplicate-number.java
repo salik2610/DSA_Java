7bf9aad2ea27d0b293c737076e8b176370528c84
Md Salik Inam <salikinam@gmail.com>
2026-08-29 00:30:49 +0530

    Time: 5 ms (62.87%) | Memory: 82.8 MB (70.92%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,12 +1,16 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer>ans = new HashSet<>();
-        for(int num:nums){
-            if(ans.contains(num)){
-                return num;
-            }
-            ans.add(num);
+        int s=nums[0];
+        int f=nums[0];
+        do{
+            s=nums[s];
+            f=nums[nums[f]];
+        }while(s!=f);
+        s=nums[0];
+        while(s!=f){
+            s=nums[s];
+            f=nums[f];
         }
-        return -1;
+        return s;
     }
 }
