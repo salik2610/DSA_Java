64ac19524be1d459fcb67b5691e164ebf302e020
Md Salik Inam <salikinam@gmail.com>
2026-08-29 00:03:32 +0530

    Time: 21 ms (34.61%) | Memory: 92.1 MB (10.28%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,21 +1,12 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-        
-        // Phase 1: cycle mein milne wala point dhoondo
-        do {
-            slow = nums[slow];          // 1 step
-            fast = nums[nums[fast]];    // 2 steps
-        } while (slow != fast);
-        
-        // Phase 2: cycle ka START (matlab duplicate number) dhoondo
-        slow = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
+        Set<Integer>ans = new HashSet<>();
+        for(int num:nums){
+            if(ans.contains(num)){
+                return num;
+            }
+            ans.add(num);
         }
-        
-        return slow;   // ye hi duplicate number hai
+        return -1;
     }
 }
