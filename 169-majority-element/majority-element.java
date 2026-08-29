153b9735e3378c23dcd5f2c9209898896c17847b
Md Salik Inam <salikinam@gmail.com>
2026-08-29 15:06:08 +0530

    Time: 15 ms (24.47%) | Memory: 52.7 MB (80.63%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int count = map.getOrDefault(nums[i],0)+1;
+            map.put(nums[i],count);
+            if(count>nums.length/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
