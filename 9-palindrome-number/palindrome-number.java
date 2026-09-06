8b6582b2b45089b9283052421aaf9fbf580fa171
Md Salik Inam <salikinam@gmail.com>
2026-09-07 02:34:29 +0530

    Time: 5 ms (82.41%) | Memory: 46.1 MB (16.13%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
         int originalNum=x;
         int reversed=0;
-        while(x>0){
+        while(x!=0){
             int digit=x%10;
             reversed=reversed*10+digit;
             x=x/10;
         }
-        if(reversed==originalNum){
-            return true;
-        }
-        return false;
+        return reversed==originalNum;
     }
 }
